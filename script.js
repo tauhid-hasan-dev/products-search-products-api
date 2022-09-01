fd38a505cd750daacbca18b0d4f1a3e4b46017ee
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-09-01 15:09:26 +0300

    search options and product loading completed

--- script.js
+++ script.js
@@ -4,18 +4,56 @@ const loadProducts = async () => {
     return data;
 }
 
-
 const setAllMenu = async () => {
     const productData = await loadProducts();
-    console.log(productData);
-    productData.forEach(product => {
-        console.log(product);
-        const { category } = product;
-        console.log(category);
-    });
     const ul = document.getElementById('search-options');
-    const li = document.createElement('li');
-    /* li.innerText = `${}` */
+    const uniqueArray = []
+    productData.forEach(product => {
+        //console.log(product);
+        const { category } = product; //
+        //console.log(category);
+        if (uniqueArray.indexOf(category) == -1) {
+            uniqueArray.push(category)
+            const li = document.createElement('li');
+            li.innerText = `${category}`
+            ul.appendChild(li);
+        }
+    });
 }
 setAllMenu()
+
+const displayProducts = async () => {
+    const productData = await loadProducts();
+    const productHolder = document.getElementById('product-holder');
+    productData.forEach(product => {
+        console.log(product);
+        const { title, price, image } = product;
+        const productCard = document.createElement('div');
+        productCard.classList.add('col-4');
+        productCard.innerHTML = `
+        <div class="card h-100 " >
+            <img src="${image}" class="card-img-top w-100 h-100 " alt="...">
+            <div class="card-body text-center">
+                <p class="card-title fs-5 fw-bolder">${title}</p>
+                <p class="card-text fw-semibold ">Price: ${price}$</p>
+                <div class="d-grid gap-2 ">
+                    <button class="btn btn-primary btn-sm fw-semibold btn-select-player"
+                        type="button" style="--bs-btn-padding-y: .6rem;" >Show Details</button>
+                </div>
+            </div>
+         </div>
+        `
+        productHolder.appendChild(productCard);
+    })
+}
+
+const searchProduct = async () => {
+    const productData = await loadProducts();
+    const inputElem = document.getElementById('product-input');
+    
+}
+
+
+
+displayProducts()
 loadProducts()
